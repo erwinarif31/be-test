ca8958aee7e093936d8358e103ec26f42a3104dd
Muhammad Erwin Arif <muherwinarif31@gmail.com>
2025-07-14 08:19:05 +0800

    feat: models

--- app/Models/Division.php
+++ app/Models/Division.php
@@ -5,6 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ *
+ * @property string $id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Division newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Division newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Division query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Division whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Division whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Division whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Division whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Division extends Model
 {
     use HasUuids;
--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -5,6 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ *
+ * @property string $id
+ * @property string $image
+ * @property string $name
+ * @property string $phone
+ * @property string $division_id
+ * @property string $position
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employee newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employee newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employee query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employee whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employee whereDivisionId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employee whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employee whereImage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employee whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employee wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employee wherePosition($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employee whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Employee extends Model
 {
     use HasUuids;
